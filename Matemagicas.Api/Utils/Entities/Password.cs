f25aa6918db858a5c7cc5ebb1b4c802ad1176c09
Matheus Colombo <111023158+mathcolombo@users.noreply.github.com>
2025-03-23 01:07:20 -0300

    Cria a classe Password e sua vaidação

--- Matemagicas.Api/Utils/Entities/Password.cs
+++ Matemagicas.Api/Utils/Entities/Password.cs
@@ -2,5 +2,25 @@ namespace Matemagicas.Api.Utils.Entities;
 
 public class Password
 {
-    
+    public string Value { get; protected set; }
+
+    public Password(string password)
+    {
+        SetPassword(password);
+    }
+
+    public void SetPassword(string password)
+    {
+        if(!IsValid(password))
+            throw new FormatException("Password invalid");
+        
+        Value = password;
+    }
+
+    private bool IsValid(string password)
+    {
+        if (string.IsNullOrEmpty(password) || !StaticParameters.PASSWORD_REGEX.IsMatch(password)) return false;
+
+        return true;
+    }
 }
